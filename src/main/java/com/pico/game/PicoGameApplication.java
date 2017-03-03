85891c4355a4dce6535e95587d006e1b808012d8
monkmonah <monkmonah@gmail.com>
2017-03-03 23:43:00 +0200

    Added spring boot

---
+++ src/main/java/com/pico/game/PicoGameApplication.java
@@ -0,0 +1,12 @@
+package com.pico.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicoGameApplication.class, args);
+    }
+}
